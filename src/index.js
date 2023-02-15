c69cb78e25758677c19a1b5a9be9b8a93f72e7c0
YuraMakhnitko <bizoru82@gmail.com>
2023-02-15 19:42:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
